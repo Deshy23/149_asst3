ea4ae905c104fe93decca2e612226f1d87165605
avidesh marajh <deshavi212@gmail.com>
2023-11-07 23:17:08 -0800

    scan-debug

--- scan/scan.cu
+++ scan/scan.cu
@@ -131,7 +131,7 @@ void exclusive_scan(int* input, int N, int* result)
     cudaDeviceSynchronize();
     // downsweep phase
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
-        int threadsPerBlock = 512;
+        int threadsPerBlock = nextPow2(N);
         int two_dplus1 = 2*two_d;
         int blocks = ((N/two_dplus1) + threadsPerBlock - 1) / threadsPerBlock;
         dsweep_kernel<<<blocks, threadsPerBlock>>>(N, device_output, two_d, two_dplus1);
