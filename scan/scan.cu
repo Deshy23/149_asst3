19d971a9cedfe724aa930e3d53dcc0397693397c
avidesh marajh <deshavi212@gmail.com>
2023-11-08 14:12:54 -0800

    scan-debug

--- scan/scan.cu
+++ scan/scan.cu
@@ -112,7 +112,7 @@ void exclusive_scan(int* input, int N, int* result)
 
     //upsweep
     for (int two_d = 1; two_d <= N/2; two_d*=2) {
-        int threadsPerBlock = 512;
+        int threadsPerBlock = 1;
         int two_dplus1 =  2 * two_d;
         int blocks = ((N/two_dplus1) + threadsPerBlock - 1) / threadsPerBlock;
         // parallel_for (int i = 0; i < N; i += two_dplus1) {
@@ -129,7 +129,7 @@ void exclusive_scan(int* input, int N, int* result)
     cudaMemcpy(&device_output[N-1], &tmp, sizeof(int), cudaMemcpyHostToDevice);
     // downsweep phase
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
-        int threadsPerBlock = 512;
+        int threadsPerBlock = 1;
         int two_dplus1 = 2*two_d;
         int blocks = ((N/two_dplus1) + threadsPerBlock - 1) / threadsPerBlock;
         dsweep_kernel<<<blocks, threadsPerBlock>>>(N, device_output, two_d, two_dplus1);
