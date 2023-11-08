abc27b7baaafbbb89d8c8c282b528ab8453648cb
avidesh marajh <deshavi212@gmail.com>
2023-11-07 19:38:52 -0800

    scan-debug

--- scan/scan.cu
+++ scan/scan.cu
@@ -128,18 +128,17 @@ void exclusive_scan(int* input, int N, int* result)
         usweep_kernel<<<blocks, threadsPerBlock>>>(N, device_input, device_output, two_d);
         cudaMemcpy(device_input, device_output, N*sizeof(int), cudaMemcpyDeviceToDevice);
     }
-    result[0] = 0;
     // device_input[N-1] = 0;
     // device_output[N-1] = 0;
 
-    // // downsweep phase
-    // for (int two_d = N/2; two_d >= 1; two_d /= 2) {
-    //     int threadsPerBlock = 512;
-    //     int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
-    //     int two_dplus1 = 2*two_d;
-    //     dsweep_kernel<<<blocks, threadsPerBlock>>>(N, device_input, device_output, two_d);
-    //     cudaMemcpy(device_input, device_output, N*sizeof(int), cudaMemcpyDeviceToDevice);
-    // }
+    // downsweep phase
+    for (int two_d = N/2; two_d >= 1; two_d /= 2) {
+        int threadsPerBlock = 512;
+        int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+        int two_dplus1 = 2*two_d;
+        dsweep_kernel<<<blocks, threadsPerBlock>>>(N, device_input, device_output, two_d);
+        cudaMemcpy(device_input, device_output, N*sizeof(int), cudaMemcpyDeviceToDevice);
+    }
 
     cudaMemcpy(result, device_output, N*sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(device_input);
