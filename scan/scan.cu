77098df9cec91294d28a53999ffcbc68dc55cbc7
avidesh marajh <deshavi212@gmail.com>
2023-11-08 20:09:53 -0800

    scan-debug

--- scan/scan.cu
+++ scan/scan.cu
@@ -127,13 +127,13 @@ void exclusive_scan(int* input, int N, int* result)
     populate_zeroes<<<rounded - N, 1>>>(N, rounded, device_output);
     cudaDeviceSynchronize();
     //upsweep
-    for (int two_d = 1; two_d < rounded/2; two_d*=2) {
+    for (int two_d = 1; two_d < N/2; two_d*=2) {
         int two_dplus1 =  2 * two_d;
-        int blocks = ((rounded/two_dplus1) + THREADS_PER_BLOCK - 1)/ THREADS_PER_BLOCK;
+        int blocks = ((N/two_dplus1) + THREADS_PER_BLOCK - 1)/ THREADS_PER_BLOCK;
         // parallel_for (int i = 0; i < N; i += two_dplus1) {
         //     output[i+two_dplus1-1] += output[i+two_d-1];
         // }
-        usweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(rounded, device_output, two_d, two_dplus1);
+        usweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(N, device_output, two_d, two_dplus1);
         cudaDeviceSynchronize();
     //     cudaMemcpy(device_input, device_output, rounded*sizeof(int), cudaMemcpyDeviceToDevice);
     }
@@ -148,10 +148,10 @@ void exclusive_scan(int* input, int N, int* result)
     populate_zeroes<<<1 + rounded - N, 1>>>(N-1, rounded, device_output);
     cudaDeviceSynchronize();
     // downsweep phase
-    for (int two_d = rounded/2; two_d >= 1; two_d /= 2) {
+    for (int two_d = N/2; two_d >= 1; two_d /= 2) {
         int two_dplus1 = 2*two_d;
-        int blocks = ((rounded/two_dplus1) + THREADS_PER_BLOCK - 1)/ THREADS_PER_BLOCK;
-        dsweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(rounded, device_output, two_d, two_dplus1);
+        int blocks = ((N/two_dplus1) + THREADS_PER_BLOCK - 1)/ THREADS_PER_BLOCK;
+        dsweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(N, device_output, two_d, two_dplus1);
         cudaDeviceSynchronize();
         // cudaMemcpy(device_input, device_output, N*sizeof(int), cudaMemcpyDeviceToDevice);
         // cudaDeviceSynchronize();
