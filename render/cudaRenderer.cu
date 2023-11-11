c17ce1a96440fac2c9cf2aa0612cce7f6e116ccb
avidesh marajh <deshavi212@gmail.com>
2023-11-11 03:14:07 -0800

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -468,7 +468,7 @@ __global__ void kernelPerBlock(){
     float B = static_cast<short>(invHeight * boxB);
 
     if(threadIdx.x == 0 && threadIdx.y == 0){
-        printf("L = %d, R = %d, T = %d, B = %d \n", L, R, T, B);
+        printf("L = %f, R = %f, T = %f, B = %f \n", L, R, T, B);
         // printf("R = %d \n", boxR);
     }
     const int numCircles = cuConstRendererParams.numCircles;
