b7c5771c9342a27bba777e15fbf7f8d41bf1ed5d
avidesh marajh <deshavi212@gmail.com>
2023-11-11 15:01:23 -0800

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -473,7 +473,7 @@ __global__ void kernelPerBlock(){
     // }
     const int numCircles = cuConstRendererParams.numCircles;
     //launch check for every circle
-    if(index == 2){
+    if(index == 0){
         
         float3 p = *(float3*)(&cuConstRendererParams.position[index3]);
         // printf("px = %f, py = %f \n", p.x, p.y);
